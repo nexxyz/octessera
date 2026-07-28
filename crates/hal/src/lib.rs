bc9963d34d6c73cbe2c59afbe3f4859d812f54f7
nexxyz <7255868+nexxyz@users.noreply.github.com>
2026-07-28 10:51:54 +0200

    Restore Raspberry encoder export

--- crates/hal/src/lib.rs
+++ crates/hal/src/lib.rs
@@ -24,7 +24,10 @@ pub mod pinmap;
 pub mod seesaw_interrupt;
 
 // Re-exports for convenience
-#[cfg(not(feature = "orange-pi-zero-2w"))]
+#[cfg(any(
+    feature = "rpi-zero-2w",
+    all(not(feature = "rpi-zero-2w"), not(feature = "orange-pi-zero-2w"))
+))]
 pub use encoder_gpio::EncoderGpio;
 #[cfg(not(feature = "orange-pi-zero-2w"))]
 pub use i2c_bus::I2CBus;
